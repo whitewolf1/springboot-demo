983a80aea2be2c8c4ad4c30806a1f89a1c7e19b6
whitewolf1 <linshengru@126.com>
2019-05-16 13:36:31 +0800

    v1

--- src/main/java/com/wiggin/springboot/demo/controller/StudentController.java
+++ src/main/java/com/wiggin/springboot/demo/controller/StudentController.java
@@ -21,7 +21,7 @@ public class StudentController {
 
     @RequestMapping(value = "/qryById")
     public Student qry(int id){
-        System.out.println("修改123");
+        System.out.println("这个是V1版");
         return service.queryById(id);
     }
 }
